d1b338044e06a8ad6c2743ff423a90b54e2f705e
Grzegorz Szaliński <grzegorz.szalinski@gmail.com>
2016-12-16 22:58:33 +0100

    Add config provider

--- web/index.php
+++ web/index.php
@@ -4,10 +4,12 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
-$app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
+$app->register(new Rpodwika\Silex\YamlConfigServiceProvider(__DIR__.'/../src/config/config.yml'));
+
+$app['debug'] = $app['config']['app']['debug'];
 
 // Controller
 $app->mount('/', new App\MainController());
