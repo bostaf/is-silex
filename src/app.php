d3617f6d3fcc8d0b7b2d6756f699ad8bf2acf549
Grzegorz Szaliński <grzegorz.szalinski@gmail.com>
2016-12-18 21:47:27 +0100

    Add error handling

--- src/app.php
+++ src/app.php
@@ -1,23 +1,26 @@
 <?php
 use Symfony\Component\Debug\ErrorHandler;
 
+
 $app->register(new Rpodwika\Silex\YamlConfigServiceProvider(__DIR__.'/../app/config/config.yml'));
+$app['debug'] = $app['config']['app']['debug'];
 
 ErrorHandler::register();
 //register an error handler
-$app->error(function ( \Exception $e, $code ) use ($app) {
-
+$app->error(function (\Exception $e, $code) use ($app) {
+    if ($app['debug']) {
+        return;
+    }
     //return your json response here
-    $error = array( 'message' => $e->getMessage() );
+    $error = array('message' => $e->getMessage(), '1' => $e->getLine(), '2' => $e->getFile());
 
-    return $app->json( $error, 200 );
+    return $app['twig']->render('_error.html.twig', []);
 });
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../app/views',
 ));
 
-$app['debug'] = $app['config']['app']['debug'];
 
 // Controller
 $app->mount('/', new Is\Controller\MainController());
