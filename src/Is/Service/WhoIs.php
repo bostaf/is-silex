a64e7c7f556083bebff1ca24694c90b3d23de322
Grzegorz Szaliński <grzegorz.szalinski@gmail.com>
2017-01-03 19:55:32 +0100

    Done with who-is management.

--- src/Is/Service/WhoIs.php
+++ src/Is/Service/WhoIs.php
@@ -17,6 +17,34 @@ class WhoIs
 
     public function getLogs()
     {
-        return array();
+        $logs = array();
+        $dir_handle = opendir($this->getDir());
+
+        while ($file = readdir($dir_handle)) {
+            if (preg_match($this->getFileRegex(), $file, $fileNameArray)) {
+                $logDate = new \DateTime($fileNameArray[1] . ' ' . $fileNameArray[2].':'.$fileNameArray[3]);
+                $logs[$fileNameArray[1]]['date'] = $logDate->format('c');
+                $logs[$fileNameArray[1]]['contents'] = file_get_contents($this->getDir().$file);
+            }
+        }
+
+        sort($logs);
+        return $logs;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDir()
+    {
+        return $this->dir;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFileRegex()
+    {
+        return $this->fileRegex;
     }
 }
