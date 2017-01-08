f456927bc856c06d5b5a8140df30f10e06595aab
Grzegorz Szaliński <grzegorz.szalinski@gmail.com>
2017-01-08 21:23:59 +0100

    Fix mobile view. Move js out of DOM.

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,9 +1,11 @@
-function toggleMenu()
-{
-    var menu = document.getElementById('main_menu');
-    if (menu.style.display == "none" || menu.style.display == "") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
+window.onload = function() {
+    var menu = document.getElementById("main_menu");
+    var menuButton = document.getElementById("menu_button");
+    menuButton.onclick = function() {
+        if (menu.style.display == "none" || menu.style.display == "") {
+            menu.style.display = "block";
+        } else {
+            menu.style.display = "none";
+        }
     }
-}
+};
