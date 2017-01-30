6620598eae09923f6254bf4c0d2c6556f74b5834
Grzegorz Szaliński <grzegorz.szalinski@gmail.com>
2017-01-30 20:46:02 +0100

    Add version to assets.

--- src/app.php
+++ src/app.php
@@ -45,9 +45,9 @@ $app->register(new Silex\Provider\AssetServiceProvider(), array(
     'assets.version' => 'v3',
     'assets.version_format' => '%s?version=%s',
     'assets.named_packages' => array(
-        'css' => array('version' => 'css3', 'base_path' => '/css'),
-        'js' => array('base_path' => '/js'),
-        'img' => array('base_path' => '/img'),
+        'css' => array('base_path' => '/css', 'version' => 'v1'),
+        'js' => array('base_path' => '/js', 'version' => 'v1'),
+        'img' => array('base_path' => '/img', 'version' => 'v1'),
     ),
 ));
 
